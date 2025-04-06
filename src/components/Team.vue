cecbe26075720b62dc50df07a0bd62b5dd300f20
Frederik Gram Kortegaard <fgk@Frederiks-MacBook-Pro.local>
2025-04-06 11:41:49 +0400

    modify copy

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -91,6 +91,7 @@
 <script>
 import teamData from '../data/team.json'
 import albertoImage from '../assets/images/team/alberto.jpg'
+import antonImage from '../assets/images/team/anton.png'
 import lasseImage from '../assets/images/team/lasse.jpg'
 
 export default {
@@ -108,6 +109,14 @@ export default {
           id,
           expanded: false,
           image: albertoImage
+        };
+      }
+      else if (id === 'anton') {
+        acc[id] = {
+          ...member,
+          id,
+          expanded: false,
+          image: antonImage
         };
       }
       else if (id === 'lasse') {
